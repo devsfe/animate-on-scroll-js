21dd4bf8a575de96295e4eb8a10e1791c5b50bd8
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-11-16 23:31:17 -0300

    16 11 finalizado

---
+++ script.js
@@ -0,0 +1,29 @@
+const dataAnime = document.querySelectorAll('[data-anime]');
+const animate =  'animate';
+
+//Eventos
+if(dataAnime.length) {
+    window.addEventListener('scroll', animeScroll);
+    console.log('teste');
+}
+
+
+
+function animeScroll() {
+    const topoDoSite = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    dataAnime.forEach(function(e){
+        if((topoDoSite) > e.offsetTop) {
+            e.classList.add(animate);
+        } else {
+            e.classList.remove(animate);
+        }
+    });
+}
+
+
+animeScroll();
+
+
+
+
+
